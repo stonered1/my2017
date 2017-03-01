54c7fe030695bad8c46e2ff97a758b8dc6769d75
Stone <reddstones@163.com>
2017-03-02 07:50:49 +0800

    My

---
+++ My/sortListView/src/main/java/stone/com/my/SortModel.java
@@ -0,0 +1,27 @@
+package stone.com.my;
+
+/**
+ * Created by Stone on 17/3/1.
+ */
+
+public class SortModel {
+
+    private String name;//显示的数据
+    private String sortLetters;//显示数据的拼音首字母
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSortLetters() {
+        return sortLetters;
+    }
+
+    public void setSortLetters(String sortLetters) {
+        this.sortLetters = sortLetters;
+    }
+}
